80efb9f78bc041bb01b9f4a1a4fd9d1c7ce27ab6
sukhanova <verusia.s@gmail.com>
2022-12-17 14:39:52 -0600

    Added solution to questions using the LIKE operator

--- BasicSQL/Lesson1.sql
+++ BasicSQL/Lesson1.sql
@@ -161,3 +161,31 @@ SELECT id, account_id,
 poster_amt_usd/(standard_amt_usd + gloss_amt_usd + poster_amt_usd) * 100 AS poster_paper_revenue
 FROM orders
 LIMIT 10;
+
+
+
+/*
+Questions using the LIKE operator
+Use the accounts table to find:
+*/
+
+/*
+All the companies whose names start with 'C'.
+*/
+SELECT name
+FROM accounts
+WHERE name LIKE 'C%';
+
+/*
+All companies whose names contain the string 'one' somewhere in the name.
+*/
+SELECT name
+FROM accounts
+WHERE name LIKE '%one%';
+
+/*
+All companies whose names end with 's'.
+*/
+SELECT name
+FROM accounts
+WHERE name LIKE '%s';
