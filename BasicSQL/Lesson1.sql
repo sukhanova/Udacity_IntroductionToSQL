6c5ee382a48fe9cfb706341ea57f5385f3f6ca35
sukhanova <verusia.s@gmail.com>
2022-12-17 14:26:33 -0600

    Added solution to Arithmetic Operations questions

--- BasicSQL/Lesson1.sql
+++ BasicSQL/Lesson1.sql
@@ -135,3 +135,29 @@ in the accounts table.
 Select name, website, primary_poc
 FROM accounts
 WHERE name = 'Exxon Mobil';
+
+
+/*
+Questions using Arithmetic Operations
+Using the orders table:
+
+Create a column that divides the standard_amt_usd by the standard_qty 
+to find the unit price for standard paper for each order. 
+Limit the results to the first 10 orders, and include the id and account_id fields.
+*/
+SELECT id, account_id, (standard_amt_usd / standard_qty) AS standart_unit_price
+FROM orders
+LIMIT 10;
+
+
+/*
+Write a query that finds the percentage of revenue that comes from poster paper 
+for each order. You will need to use only the columns that end with _usd. 
+(Try to do this without using the total column.) 
+Display the id and account_id fields also.
+*/
+
+SELECT id, account_id,
+poster_amt_usd/(standard_amt_usd + gloss_amt_usd + poster_amt_usd) * 100 AS poster_paper_revenue
+FROM orders
+LIMIT 10;
